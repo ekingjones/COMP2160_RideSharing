b6899eaa7ddc1644e15bf693def46879b4da17b3
Eric King-Jones <ekingjones@gmail.com>
2018-10-15 19:17:48 -0700

    Initial commit

---
+++ app/src/main/java/com/comp2160/robot/comp2160_ridesharing/OnboardActivity.java
@@ -0,0 +1,13 @@
+package com.comp2160.robot.comp2160_ridesharing;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OnboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboard);
+    }
+}
