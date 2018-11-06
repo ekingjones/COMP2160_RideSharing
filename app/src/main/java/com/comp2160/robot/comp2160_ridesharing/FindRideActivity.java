2a07b9a861d46b56e0f22e34144d8acdc7f1cc64
Eric King-Jones <ekingjones@gmail.com>
2018-11-06 10:45:06 -0800

    Added login activity, revised find trip layout

--- app/src/main/java/com/comp2160/robot/comp2160_ridesharing/FindRideActivity.java
+++ app/src/main/java/com/comp2160/robot/comp2160_ridesharing/FindRideActivity.java
@@ -10,23 +10,21 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
-import java.sql.Driver;
-
-public class DriverStartActivity extends AppCompatActivity {
+public class FindRideActivity extends AppCompatActivity {
     private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_driver_start);
+        setContentView(R.layout.activity_find_ride);
 
-        // define layout for mDrawerLayout from activity_driver_start layout
+        // define layout for mDrawerLayout from activity_onboard layout
         mDrawerLayout = findViewById(R.id.drawer_layout);
 
         // used to set custom toolbar as action bar
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        toolbar.setSubtitle("Start a Ride");         // *add this to string file
+        toolbar.setSubtitle("Where to?");         // TODO *add this to string file
         ActionBar actionbar = getSupportActionBar();
         actionbar.setDisplayHomeAsUpEnabled(true);
         actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
@@ -37,37 +35,46 @@ public class DriverStartActivity extends AppCompatActivity {
                 new NavigationView.OnNavigationItemSelectedListener() {
                     @Override
                     public boolean onNavigationItemSelected(MenuItem menuItem) {
-
+                        // switch to handle navigation between activities
                         int id = menuItem.getItemId();
                         switch (id) {
                             case(R.id.nav_1):
-                                Intent i1 = new Intent(DriverStartActivity.this, FindRideActivity.class);
-                                startActivity(i1);
-                                break;
-                            case(R.id.nav_2):
+                                // nav_1 is this current activity, so instead of reloading the activity
+                                // with an intent, simply close the drawer and take the user back to the screen
                                 mDrawerLayout.closeDrawers();
                                 break;
+                            case(R.id.nav_2):
+                                // nav_2 is the DriverStartActivity, so when this item is selected
+                                // the DriverStart activity is started
+                                Intent i2 = new Intent(FindRideActivity.this, DriverStartActivity.class);
+                                startActivity(i2);
+                                break;
                             case(R.id.nav_3):
-                                //Intent i3 = new Intent(DriverStartActivity.this, trip_activity.class);
+                                // TODO add when  trips layout is complete
+                                //Intent i3 = new Intent(FindRideActivity.this, trip_layout);
                                 //startActivity(i3);
                                 break;
                             case(R.id.nav_4):
-                                //Intent i4 = new Intent(DriverStartActivity.this, settings_activity.class);
+                                // TODO add when setting layout is complete
+                                //Intent i4 = new Intent(FindRideActivity.this, settings_layout);
                                 //startActivity(i4);
                                 break;
+
                         }
+                        // closes drawer after item is selected
+                        mDrawerLayout.closeDrawers();
                         return true;
                     }
                 });
     }
 
+    // this opens the menu when the icon in the toolbar is clicked
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
                 mDrawerLayout.openDrawer(GravityCompat.START);
                 return true;
-
         }
         return super.onOptionsItemSelected(item);
     }
---
+++ app/src/main/java/com/comp2160/robot/comp2160_ridesharing/LoginActivity.java
@@ -0,0 +1,64 @@
+package com.comp2160.robot.comp2160_ridesharing;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button button_1,button_2;
+    EditText edit_1,edit_2;
+
+    TextView text_1;
+    int counter = 3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        button_1 = (Button)findViewById(R.id.button);
+        edit_1 = (EditText)findViewById(R.id.editText);
+        edit_2 = (EditText)findViewById(R.id.editText2);
+
+        button_2 = (Button)findViewById(R.id.button2);
+        text_1 = (TextView)findViewById(R.id.textView3);
+        text_1.setVisibility(View.GONE);
+
+        button_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(edit_1.getText().toString().equals("admin") &&
+                        edit_2.getText().toString().equals("admin")) {
+                    Toast.makeText(getApplicationContext(),
+                            "Redirecting...",Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(), "Incorrect Information.",
+                            Toast.LENGTH_SHORT).show();
+
+                    text_1.setVisibility(View.VISIBLE);
+                    text_1.setBackgroundColor(Color.RED);
+                    counter--;
+                    text_1.setText(Integer.toString(counter));
+
+                    if (counter == 0) {
+                        button_1.setEnabled(false);
+                    }
+                }
+            }
+        });
+
+        button_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+}
