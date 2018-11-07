c2169ba4b0522c38a9a3458f08eb808bb03d2066
Eric King-Jones <ekingjones@gmail.com>
2018-11-06 17:50:42 -0800

    Added login activity, revised find trip layout

--- app/src/main/java/com/comp2160/robot/comp2160_ridesharing/DriverStartActivity.java
+++ app/src/main/java/com/comp2160/robot/comp2160_ridesharing/DriverStartActivity.java
@@ -1,18 +1,24 @@
 package com.comp2160.robot.comp2160_ridesharing;
 
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.format.DateFormat;
 import android.view.MenuItem;
+import android.widget.TimePicker;
 
 import java.sql.Driver;
+import java.util.Calendar;
 
-public class DriverStartActivity extends AppCompatActivity {
+public class DriverStartActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
     private DrawerLayout mDrawerLayout;
 
     @Override
@@ -26,7 +32,7 @@ public class DriverStartActivity extends AppCompatActivity {
         // used to set custom toolbar as action bar
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        toolbar.setSubtitle("Start a Ride");         // *add this to string file
+        toolbar.setSubtitle("Start a Ride");         // TODO add this to string file
         ActionBar actionbar = getSupportActionBar();
         actionbar.setDisplayHomeAsUpEnabled(true);
         actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
@@ -70,5 +76,25 @@ public class DriverStartActivity extends AppCompatActivity {
 
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    // TODO fix this mess to add time picker, and add date picker as well
+    /*@Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current time as the default values for the picker
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
+        // Create a new instance of TimePickerDialog and return it
+        return new TimePickerDialog(getActivity(), this, hour, minute,
+                DateFormat.is24HourFormat(getActivity()));
+    }*/
+
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        // this method will do something with the time the user picks
+        // probably set the text of the label that they touch
     }
 }
